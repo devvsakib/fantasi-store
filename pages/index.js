a92cf90e73d30f2f9bc5fb5281b0c92652457ee0
DevvSakib <devvsakib@gmail.com>
2023-04-27 11:09:34 +0600

    loader added

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,21 @@ import HeroSection from "@/components/HeroSection";
 import ProductCard from "@/components/ProductCard";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Image from "next/image";
 
 export default function Home() {
   const [products, setProducts] = useState([])
   const [categories, setCategories] = useState([])
-
+  const [loading, setLoading] = useState(true)
   const fetchData = async () => {
     try {
+      setLoading(true);
       const productResponse = await axios.get('https://fantasiserver.onrender.com');
       const categoryResponse = await axios.get('https://fantasiserver.onrender.com/categories');
 
       setProducts(productResponse.data);
       setCategories(categoryResponse.data);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -32,6 +35,15 @@ export default function Home() {
       <Layout>
         <div className="my-20">
           <h3 className="font-semibold text-xl">Explore Brands</h3>
+          <div>
+            {
+              loading && <Image
+                src={"/spinner.svg"}
+                width={100}
+                height={100}
+                className="mx-auto"
+              />}
+          </div>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 mt-5">
             {
               categories?.map(category => <Category key={category.id} category={category} />)
@@ -39,6 +51,15 @@ export default function Home() {
           </div>
           <div className="my-32">
             <h1 className="mb-5 bg-clip-text text-transparent bg-gradient-to-tr to-teal-400 from-indigo-600 text-xl md:text-3xl font-bold text-center">Awesome Products</h1>
+            <div>
+              {
+                loading && <Image
+                  src={"/spinner.svg"}
+                  width={100}
+                  height={100}
+                  className="mx-auto"
+                />}
+            </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 mt-5">
               {
                 products.map((product, idx) => <ProductCard key={idx} product={product} />)
